87385693a1036d942e82d7b1e9c8e2d658c1ccf1
Matthias Käppler <m.kaeppler@gmail.com>
2010-07-09 16:43:21 +0200

    code cleanup

--- src/main/java/com/github/calculon/assertion/UserInputAssertionBase.java
+++ src/main/java/com/github/calculon/assertion/UserInputAssertionBase.java
@@ -26,25 +26,7 @@ public abstract class UserInputAssertionBase<TargetT, ActivityT extends Activity
         }, false);
     }
 
-//    public ActionAssertion clickMenuItem(int menuItemIndex) {
-//    	final int itemIndex = menuItemIndex;
-//        return new ActionAssertion(activity, instrumentation, new Runnable() {
-//            public void run() {
-//            	/**
-//                UserInputAssertionBase.this.instrumentation.sendKeySync(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_MENU));
-//                for (int i = 0; i<itemIndex; i++) {
-//                    UserInputAssertionBase.this.instrumentation.sendKeySync(new KeyEvent(
-//                            KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_MENU));
-//                }
-//                UserInputAssertionBase.this.instrumentation.sendKeySync(new KeyEvent(
-//                        KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_DPAD_DOWN));
-//                */
-//            	
-//            }
-//        }, true);
-//    }
-
-    public void satisfies(Predicate<T> predicate) {
+    public void satisfies(Predicate<TargetT> predicate) {
         try {
             assertTrue(predicate.check(target));
         } catch (Exception e) {
