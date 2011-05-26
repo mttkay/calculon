b821b19eae68788b93dffc2fb534c844f48a4723
Stefano Dacchille <stefano@qype.com>
2011-05-26 16:48:56 +0200

    Changed the return type to the generic type.

--- src/main/java/com/github/calculon/CalculonUnitTest.java
+++ src/main/java/com/github/calculon/CalculonUnitTest.java
@@ -29,7 +29,7 @@ public abstract class CalculonUnitTest<ActivityT extends Activity> extends
     }
 
     @Override
-    public Activity getCurrentActivity() {
+    public ActivityT getCurrentActivity() {
         return getActivity();
     }
 
